29e692f6d138341250123314e73197f7d78c1892
Mark Dearoz <markdearoz08@gmail.com>
2021-04-19 21:10:31 -0400

    skills animation done

---
+++ src/animations/skills.js
@@ -0,0 +1,61 @@
+
+const PERCENT = [90, 90, 75, 75, 65, 85]
+const windowWidth = window.innerWidth
+const h1 = document.getElementsByClassName("skills-h1")
+const circleBars = document.getElementsByClassName("circle-bar")
+const circleProgressBars = document.getElementsByClassName("circle-progress")
+const circleTitles = document.getElementsByClassName("circle-title")
+
+
+export const animateSkills = () =>{
+    for(let i = 0; i < circleBars.length; i++){
+        circleBars[i].style.transition = "opacity 200ms ease-in-out 750ms"
+        circleTitles[i].style.transition = "opacity 500ms ease-in-out"
+        circleTitles[i].style.transitionDelay = `${((i+1) * 125) + 750}ms`
+        circleBars[i].style.opacity = "100%"
+        circleTitles[i].style.opacity = "100%"
+    }
+    for(let i = 0; i < circleProgressBars.length; i++){
+        circleProgressBars[i].style.transition = "stroke-dashoffset 200ms ease-in-out"
+        circleProgressBars[i].style.transitionDelay = `${((i+1) * 125) + 750}ms`
+        circleProgressBars[i].style.strokeDashoffset = (100 * 2 * 3.14 - (PERCENT[i] / 100 * 100 * 2 * 3.14))
+        
+    }
+
+    h1[0].style.transition =  "transform 750ms ease-in-out 1000ms"
+    if(windowWidth > 1200){
+        h1[0].style.transform = "rotate(90deg) translateY(-178px) translateX(0px)"
+    }
+    if(windowWidth <= 1200 && windowWidth > 600){
+        h1[0].style.transform = "rotate(90deg) translateY(-148px) translateX(0px)"
+    } 
+    if(windowWidth <= 600){
+        h1[0].style.transform = "rotate(90deg) translateY(-118px) translateX(0px)"
+    }
+}
+
+export const deanimateSkills = () => {
+    for(let i = 0; i < circleBars.length; i++){
+        circleBars[i].style.transition = "opacity 200ms ease-in-out 375ms"
+        circleTitles[i].style.transition = "opacity 175ms ease-in-out"
+        circleTitles[i].style.transitionDelay = `${((i+1) * 100)}ms`
+        circleBars[i].style.opacity = "0%"
+        circleTitles[i].style.opacity = "0%"
+    }
+    for(let i = 0; i < circleProgressBars.length; i++){
+        circleProgressBars[i].style.transition = "stroke-dashoffset 200ms ease-in-out"
+        circleProgressBars[i].style.transitionDelay = `${((i+1) * 100)}ms`
+        circleProgressBars[i].style.strokeDashoffset = (100 * 2 * 3.14 - (0 / 100 * 100 * 2 * 3.14))
+    }
+    h1[0].style.transition =  "transform 750ms ease-in-out 0ms"
+    if(windowWidth > 1200){
+        h1[0].style.transform = "rotate(90deg) translateY(-178px) translateX(640px)"
+    }
+    if(windowWidth <= 1200 && windowWidth > 600){
+        h1[0].style.transform = "rotate(90deg) translateY(-148px) translateX(640px)"
+    } 
+    if(windowWidth <= 600){
+        h1[0].style.transform = "rotate(90deg) translateY(-118px) translateX(640px)"
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
+import {animateSkills, deanimateSkills} from './animations/skills.js'
 
+const HEIGHT = 640;
 const navCircles = document.getElementsByClassName("nav-circle")
-
+const sectionContainer = document.getElementsByClassName("section-container")
 
 navCircles[0].style.opacity = "100%"
+
+const navCircleClickHandler = index => {
+    sectionContainer[0].style.transitionDelay = "750ms"
+    sectionContainer[0].style.transform = `translateY(-${index * HEIGHT}px)`
+    for(let i = 0; i < navCircles.length; i++){
+        if(i === index) navCircles[i].style.opacity = "100%"
+        if(i !== index) navCircles[i].style.opacity = "50%"
+
+    }
+    if(index === 0){
+        deanimateSkills()
+    }
+    if(index === 1){
+        animateSkills()
+    }
+    if(index === 2){
+        deanimateSkills()
+    }
+    if(index === 3){
+        deanimateSkills()
+    }
+}
+
+for(let i = 0; i < navCircles.length; i++) {
+    navCircles[i].addEventListener("click",() =>  navCircleClickHandler(i))
+}
